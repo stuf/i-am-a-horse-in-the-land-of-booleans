fdec9a6380939acc5492a9252aa8ec42d8ccd778
Stefan Rimaila <stefan@rimaila.fi>
2017-02-04 23:38:58 +0200

    Finish exercise 7

--- src/i_am_a_horse_in_the_land_of_booleans.clj
+++ src/i_am_a_horse_in_the_land_of_booleans.clj
@@ -16,16 +16,23 @@
     false))
 
 (defn fizzbuzz [n]
-  ":(")
+  (cond (divides? 15 n) "gotcha!"
+        (divides? 3 n) "fizz"
+        (divides? 5 n) "buzz"
+        :else ""))
 
 (defn teen? [age]
   (<= 13 age 19))
 
 (defn not-teen? [age]
-  ":(")
+  (not (teen? age)))
 
 (defn generic-doublificate [x]
-  ":(")
+  (cond (number? x)  (* 2 x)
+        (empty? x)   nil
+        (list? x)    (* 2 (count x))
+        (vector? x)  (* 2 (count x))
+        :else        true))
 
 (defn leap-year? [year]
   ":(")
